59a1e0ae0a07805ea49127a84c0bae4339e0aac0
Subhajit Saha <s.subhajit89@gmail.com>
2020-03-14 11:49:23 +0530

    Integration of Gallery app with container app

--- ContainerApp/src/app/app.component.ts
+++ ContainerApp/src/app/app.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare function linkAppAssets(): any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  kartChangedListener(event) {
-    console.log('from conatiner', event);
+export class AppComponent implements OnInit {
+
+  itemCount: number = 0;
+
+  ngOnInit() {
+    linkAppAssets();
+  }
+
+  kartChangedListener(event: CustomEvent) {
+    this.itemCount = event.detail.length;
   }
 }
--- ContainerApp/src/assets/linker.js
+++ ContainerApp/src/assets/linker.js
@@ -1,5 +1,31 @@
-var linkAppAssets = function (appId, callback) {
-    fetch('http://127.0.0.1:5500/dist/FruitGalleryApp/manifest.json').then((response) => {
-        console.log(response);
+function linkAppAssets() {
+    const rootURL = 'http://127.0.0.1:5500/dist/FruitGalleryApp/';
+    fetch(rootURL + 'manifest.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((parsedJson) => {
+        //const parsedJson = JSON.parse(data);
+        Object.keys(parsedJson).forEach(x => {
+            //skip polyfills
+            const polyfillsRex = /^polyfills/g;
+            if(!polyfillsRex.test(x)) {
+                const cssRex = /.css$/g;
+                if(cssRex.test(x)) {
+                    //Append stylesheet
+                    const styles = document.createElement('link');
+                    styles.setAttribute('rel', 'stylesheet');
+                    styles.setAttribute('href', rootURL + parsedJson[x]);
+                    document.head.appendChild(styles);
+                } else {
+                    //Append scripts
+                    const scripts = document.createElement('script');
+                    scripts.setAttribute('type', 'text/javascript');
+                    scripts.setAttribute('src', rootURL + parsedJson[x]);
+                    scripts.setAttribute('defer', true);
+                    document.body.appendChild(scripts);
+                }
+            }
+        });
     });
 }
